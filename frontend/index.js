ee61da546c77d59e8fa2f8d7383c4e30f09bb3b0
shiiyan <yan39123@gmail.com>
2021-09-09 20:30:49 +0900

    create ul node & li node

--- frontend/index.js
+++ frontend/index.js
@@ -1,21 +1,41 @@
 const getInputUrl = () => document.getElementById("url").value;
 
-const showResult = (htmlString) => {
-  document.getElementById("result").innerHTML = htmlString;
+const parseKeywordFromURL = (url) => {
+  const segments = new URL(url).pathname.split("/");
+  return decodeURI(segments.pop());
 };
 
+const appendKeywordToList = (wikiURL) => {
+  const ulNode = document.createElement("ul")
+  const keywordText = document.createTextNode(parseKeywordFromURL(wikiURL));
+  const liNode = document.createElement("li");
+  liNode.appendChild(keywordText);
+  ulNode.appendChild(liNode);
+  return ulNode
+};
+
+// TODO: replace local host with real cors proxy server.
 const fetchHtml = (url) => fetch("http://localhost:8080/" + url);
 
 const main = () => {
   const url = getInputUrl();
-  fetchHtml(url)
-    .then((response) => {
-      return response.text();
-    })
-    .then((responseInHtmlString) => {
-      showResult(responseInHtmlString);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  const rootUlNode = document.getElementById("result");
+  // appendKeywordToList(rootUlNode, url);
+
+  const appendKeywordToList(rootUlNode, "https://ja.wikipedia.org/wiki/Foobar")
+
+
+  rootUlNode.firstChild.
+  appendKeywordToList(, "https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%BF%E6%A7%8B%E6%96%87%E5%A4%89%E6%95%B0")
+
+  // fetchHtml(url)
+  //   .then((response) => {
+  //     return response.text();
+  //   })
+  //   .then((responseInHtmlString) => {
+  //     showResult(responseInHtmlString);
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //   });
 };
