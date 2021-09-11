6cd3a9a413265a73690c2d159a13aeba70f6c942
shiiyan <yan39123@gmail.com>
2021-09-11 16:52:12 +0900

    add function fetchKeywordsFromWikipedia

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,62 @@
 const getInputUrl = () => document.getElementById("url").value;
 
+// TODO: replace local host with real cors proxy server.
+const fetchHtmlWithCorsProxy = (url) => fetch("http://localhost:8080/" + url);
+
 const parseKeywordFromURL = (url) => {
   const segments = new URL(url).pathname.split("/");
   return decodeURI(segments.pop());
 };
 
-// TODO: replace local host with real cors proxy server.
-const fetchHtmlWithCorsProxy = (url) => fetch("http://localhost:8080/" + url);
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const fetchKeywordsFromWikipedia = async (url) => {
+  await sleep(1000);
+
+  const host = "https://" + new URL(url).host;
+  const response = await fetchHtmlWithCorsProxy(url);
+  const responseHtmlString = await response.text();
+
+  const responseDoc = new DOMParser().parseFromString(
+    responseHtmlString,
+    "text/html"
+  );
+
+  const p = responseDoc
+    .querySelector("div.mw-parser-output")
+    .querySelector("p");
+  const anchorList = p.querySelectorAll('[href*="/wiki/"]');
+
+  const fetchedKeywords = [...anchorList].map((a) => ({
+    keyword: parseKeywordFromURL(a.href),
+    parentKeyword: parseKeywordFromURL(url),
+    url: host + a.attributes.href.value,
+  }));
+
+  return fetchedKeywords;
+};
+
+const generateKeywordList = (url) => {
+  const keywordList = [];
+  keywordList.push({
+    keyword: parseKeywordFromURL(url),
+    parentKeyword: null,
+    url: url,
+  });
+
+  var i = 0;
+  while (i < 20) {
+    const keywords = fetchKeywordsFromWikipedia(keywordList[i].url);
+    keywordList.push(...keywords);
+    i += 1;
+  }
+
+  return keywordList;
+};
 
 // TODO: change below to real documental comment
 // Sample result List
-// const resultList = [
+// const keywordList = [
 // 	{ keyword: "Foobar", parentKeyword: null },
 // 	{ keyword: "メタ構文変数", parentKeyword: "Foobar" },
 // 	{ keyword: "プログラミング言語", parentKeyword: "メタ構文変数" },
@@ -18,7 +64,7 @@ const fetchHtmlWithCorsProxy = (url) => fetch("http://localhost:8080/" + url);
 // ];
 
 // Sample result Tree
-// const resultTree = {
+// const keywordTree = {
 //   keyword: "Foobar",
 //   children: [
 //     {
@@ -31,61 +77,47 @@ const fetchHtmlWithCorsProxy = (url) => fetch("http://localhost:8080/" + url);
 //   ],
 // };
 
-const transformResultListToTree = (resultList) => {
-  let resultTree;
-  resultList.forEach((one) => {
+const transformListToTree = (keywordList) => {
+  let keywordTree;
+  keywordList.forEach((one) => {
     if (!one.parentKeyword) {
-      resultTree = one;
+      keywordTree = one;
       return;
     }
 
-    const parentItem = resultList.find(
+    const parentItem = keywordList.find(
       (another) => another.keyword === one.parentKeyword
     );
     parentItem.children = [...(parentItem.children || []), one];
   });
 
-  return resultTree;
+  return keywordTree;
 };
 
-const transformResultTreeToHtmlList = (resultTree, container) => {
+const transformTreeToHtmlList = (keywordTree, container) => {
   const li = document.createElement("li");
   container.appendChild(li);
-  li.innerHTML = resultTree.keyword;
+  li.innerHTML = keywordTree.keyword;
 
-  resultTree.children?.forEach((subTree) => {
+  keywordTree.children?.forEach((subTree) => {
     const ul = document.createElement("ul");
     li.appendChild(ul);
-    transformResultTreeToHtmlList(subTree, ul);
+    transformTreeToHtmlList(subTree, ul);
   });
 };
 
-const main = () => {
+const main = async () => {
   try {
-    // const url = getInputUrl();
-    // const resultList = fetchKeywords(url);
+    const url = getInputUrl();
+    const fetchedKeywords = await fetchKeywordsFromWikipedia(url);
 
-    const resultList = [
-      { keyword: "Foobar", parentKeyword: null },
-      { keyword: "メタ構文変数", parentKeyword: "Foobar" },
-      { keyword: "プログラミング言語", parentKeyword: "メタ構文変数" },
-    ];
+    console.log("fetchedKeywords, ", fetchedKeywords);
+    // const keywordList = generateKeywordList(url);
 
-    const resultTree = transformResultListToTree(resultList);
+    // const keywordTree = transformListToTree(keywordList);
 
-    const rootContainer = document.getElementById("result");
-    transformResultTreeToHtmlList(resultTree, rootContainer);
-
-    // fetchHtml(url)
-    //   .then((response) => {
-    //     return response.text();
-    //   })
-    //   .then((responseInHtmlString) => {
-    //     showResult(responseInHtmlString);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
+    // const rootContainer = document.getElementById("result");
+    // transformTreeToHtmlList(keywordTree, rootContainer);
   } catch (err) {
     console.error(err);
   }
